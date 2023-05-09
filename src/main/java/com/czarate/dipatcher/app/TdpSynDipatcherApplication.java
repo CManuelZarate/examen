4f96923d1a61c574e624a88d3621de0c606aa563
“CManuelZarate” <camazala@gmail.com>
2023-05-09 11:29:31 -0500

    primer commit

---
+++ src/main/java/com/czarate/dipatcher/app/TdpSynDipatcherApplication.java
@@ -0,0 +1,13 @@
+package com.czarate.dipatcher.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdpSynDipatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdpSynDipatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/czarate/dipatcher/app/TdpSynDipatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.czarate.dipatcher.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdpSynDipatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
